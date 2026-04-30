91c9b21e966c7c4549b03a62611e55f15145fd37
Rafael Coelho <coelhorafael512@gmail.com>
2026-04-29 21:41:56 -0300

    Poo7 - @property

---
+++ POO/poo7.py
@@ -0,0 +1,24 @@
+# @property - um getter no modo Pythônico
+# getter - um método para obter um atributo
+# cor -> get_cor()
+# modo pythônico - modo do Python de fazer coisas
+# @property é uma propriedade do objeto, ela
+# é um método que se comporta como um
+# atributo 🤯 🤯 🤯
+# Geralmente é usada nas seguintes situações:
+# - como getter
+# - p/ evitar quebrar código cliente
+# - p/ habilitar setter
+# - p/ executar ações ao obter um atributo
+# Código cliente - é o código que usa seu código
+
+class Pen:
+    def __init__(self, color):
+        self._color = color
+
+    @property
+    def color(self):
+        return self._color
+    
+p1 = Pen('Blue')
+print(p1.color)
