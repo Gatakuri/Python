62aee9e6e42bb5476f58632735214018bec1c7b1
Coelho DEV <138379688+OCoelho-DEV@users.noreply.github.com>
2026-05-06 19:59:46 -0300

    Atualizar o blende_autosave_rafael.py

--- exercicios/blende_autosave_rafael.py
+++ exercicios/blende_autosave_rafael.py
@@ -1,22 +1,25 @@
 bl_info = {
-    "name": "Auto Save by CoelhoDEV",
+    "name": "Auto Save Like VSCode",
     "blender": (4, 0, 0),
     "category": "System",
 }
 
 import bpy
 import time
+from bpy.app.handlers import persistent
 
 last_change = time.time()
 last_save = time.time()
 
 SAVE_DELAY = 5
 
+@persistent
 def on_change(scene, depsgraph):
     global last_change
     last_change = time.time()
+    print("Mudança detectada")
 
-def autosave_timer():
+def autosave():
     global last_save
 
     now = time.time()
@@ -24,18 +27,22 @@ def autosave_timer():
     if now - last_change > SAVE_DELAY:
         if now - last_save > SAVE_DELAY:
             if bpy.data.is_saved:
-                bpy.ops.wm.save_mainfile()
+                bpy.ops.wm.save_mainfile(check_existing=False)
                 print("Auto saved")
                 last_save = now
 
-    return 1
+    return 1.0
 
 def register():
+    print("Addon iniciado")
+
     if on_change not in bpy.app.handlers.depsgraph_update_post:
         bpy.app.handlers.depsgraph_update_post.append(on_change)
 
-    bpy.app.timers.register(autosave_timer)
+    bpy.app.timers.register(autosave)
 
 def unregister():
+    print("Addon removido")
+
     if on_change in bpy.app.handlers.depsgraph_update_post:
         bpy.app.handlers.depsgraph_update_post.remove(on_change)
