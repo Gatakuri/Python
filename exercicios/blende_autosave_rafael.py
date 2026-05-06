c91cacd367023c6582bb4117d2b1ad9e56eaa252
Coelho DEV <138379688+OCoelho-DEV@users.noreply.github.com>
2026-05-06 19:43:03 -0300

    Atualizar o blende_autosave_rafael.py

--- exercicios/blende_autosave_rafael.py
+++ exercicios/blende_autosave_rafael.py
@@ -1,24 +1,41 @@
+bl_info = {
+    "name": "Auto Save Like VSCode",
+    "blender": (4, 0, 0),
+    "category": "System",
+}
+
 import bpy
 import time
 
-last_save = 0
-save_interval = 10  # segundos
+last_change = time.time()
+last_save = time.time()
 
-def autosave(scene, depsgraph):
+SAVE_DELAY = 5
+
+def on_change(scene, depsgraph):
+    global last_change
+    last_change = time.time()
+
+def autosave_timer():
     global last_save
 
-    current_time = time.time()
+    now = time.time()
 
-    # evita salvar toda hora
-    if current_time - last_save > save_interval:
-        if bpy.data.is_saved:
-            bpy.ops.wm.save_mainfile()
-            print("Projeto salvo automaticamente!")
-            last_save = current_time
+    if now - last_change > SAVE_DELAY:
+        if now - last_save > SAVE_DELAY:
+            if bpy.data.is_saved:
+                bpy.ops.wm.save_mainfile()
+                print("Auto saved")
+                last_save = now
 
-# remove duplicados
-for h in bpy.app.handlers.depsgraph_update_post:
-    if h.__name__ == "autosave":
-        bpy.app.handlers.depsgraph_update_post.remove(h)
+    return 1
 
-bpy.app.handlers.depsgraph_update_post.append(autosave)
+def register():
+    if on_change not in bpy.app.handlers.depsgraph_update_post:
+        bpy.app.handlers.depsgraph_update_post.append(on_change)
+
+    bpy.app.timers.register(autosave_timer)
+
+def unregister():
+    if on_change in bpy.app.handlers.depsgraph_update_post:
+        bpy.app.handlers.depsgraph_update_post.remove(on_change)
